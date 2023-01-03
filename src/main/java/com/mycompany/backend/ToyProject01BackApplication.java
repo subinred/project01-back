b2eb461489c6285b02bfd9846b1eae9d6f34ee2f
subinred <bleeker10@naver.com>
2023-01-03 21:10:08 +0900

    test commit

--- src/main/java/com/mycompany/backend/ToyProject01BackApplication.java
+++ src/main/java/com/mycompany/backend/ToyProject01BackApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class ToyProject01BackApplication {
+import lombok.extern.log4j.Log4j2;
 
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Log4j2
+public class ToyProject01BackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToyProject01BackApplication.class, args);
+		log.info("run");
 	}
-
 }
